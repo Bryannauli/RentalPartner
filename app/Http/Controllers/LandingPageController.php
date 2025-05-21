f445a07e6ccc9fc5077d113af1a931da7a03acb6
Agnes Olivia <agnesketaren94@gmail.com>
2025-05-21 12:58:08 +0700

    hubungkan halaman login

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -7,9 +7,22 @@ use Illuminate\Http\Request;
 
 class LandingPageController extends Controller
 {
+    public function main()
+    {
+        return view('landing.main');
+    }
+
+    public function login(){
+        return view('landing.login');
+    }
+
     public function index()
     {
-         $cars = Car::all();
-     return view('landing.index', compact('cars'));
+    $cars = Car::all();
+    return view('landing.index', compact('cars'));
     }
 }
+
+
+
+
